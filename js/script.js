6a22415e6096c453213457d9e86baeb67d0db6c4
Jacob Moreno <jacobmoreno950@gmail.com>
2022-09-13 11:17:18 -0700

    finished challenge-3

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Using the .length property, console log the number of letters in “Supercalifragilisticexpialidocious”
+let longString = "supercalifragilisticexpialidocious"
+console.log(longString)
+console.log(typeof longString)
+
+console.log(longString.length)
+console.log(typeof longString.length)
+
+// console log any number
+let anyNum = 9999999999999999999999999999999;
+
+console.log(anyNum)
+console.log(typeof anyNum)
+
+// console log a BigInt
+const bigInt = 9999999999999999999999999999999n;
+
+console.log(bigInt)
+console.log(typeof bigInt)
